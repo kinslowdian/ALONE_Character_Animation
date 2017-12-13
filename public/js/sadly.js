58b95a44d2ec9e0b9d0fe3c7d7121e5c75a3d268
kinslowdian <simonkinslow@gmail.com>
2017-12-13 08:57:29 +0000

    Eye edited to allow directions with css and js

--- public/js/sadly.js
+++ public/js/sadly.js
@@ -16,9 +16,11 @@ class Sadly
 		this.body_head = this.htmlAttach.querySelector(".sadly-head");
 		this.body_jawT = this.htmlAttach.querySelector(".sadly-jawT");
 		this.body_jawB = this.htmlAttach.querySelector(".sadly-jawB");
+		this.body_eye = this.htmlAttach.querySelector(".sadly-pupil");
 	
 		this.body_head_css = false;
 		this.body_jaw_css = false;
+		this.body_eye_css = false;
 	}
 
 	createActions(num, classKit)
@@ -26,10 +28,12 @@ class Sadly
 		this.settings["action" + num] = {};
 		this.settings["action" + num].headCSS = classKit.head;
 		this.settings["action" + num].jawCSS = classKit.jaw;
+		this.settings["action" + num].eyeCSS = classKit.eye;
 	}
 
 	applyAction(num)
 	{
+		// HEAD
 		if(this.body_head_css)
 		{
 			this.body_head.classList.remove(this.body_head_css);
@@ -38,6 +42,7 @@ class Sadly
 		this.body_head.classList.add(this.settings["action" + num].headCSS);
 		this.body_head_css = this.settings["action" + num].headCSS;
 
+		// JAW
 		if(this.body_jaw_css)
 		{
 			this.body_jawT.classList.remove(this.body_jaw_css);
@@ -47,6 +52,15 @@ class Sadly
 		this.body_jawT.classList.add(this.settings["action" + num].jawCSS);
 		this.body_jawB.classList.add(this.settings["action" + num].jawCSS);
 		this.body_jaw_css = this.settings["action" + num].jawCSS;
+	
+		// EYE
+		if(this.body_eye_css)
+		{
+			this.body_eye.classList.remove(this.body_eye_css);
+		}
+
+		this.body_eye.classList.add(this.settings["action" + num].eyeCSS);
+		this.body_eye_css = this.settings["action" + num].eyeCSS;
 	}
 }
 
@@ -71,10 +85,10 @@ function init_sadly()
 
 	sadly = new Sadly(displayList.sadly);
 
-	sadly.createActions(0, {head: "setting-head-0", jaw: "setting-jaw-0"});
-	sadly.createActions(1, {head: "setting-head-1", jaw: "setting-jaw-1"});
-	sadly.createActions(2, {head: "setting-head-2", jaw: "setting-jaw-2"});
-	sadly.createActions(3, {head: "setting-head-3", jaw: "setting-jaw-3"});
+	sadly.createActions(0, {head: "setting-head-0", jaw: "setting-jaw-0", eye: "setting-eye-F"});
+	sadly.createActions(1, {head: "setting-head-1", jaw: "setting-jaw-1", eye: "setting-eye-D"});
+	sadly.createActions(2, {head: "setting-head-2", jaw: "setting-jaw-2", eye: "setting-eye-U"});
+	sadly.createActions(3, {head: "setting-head-3", jaw: "setting-jaw-3", eye: "setting-eye-B"});
 
 	trace(sadly);
 }
